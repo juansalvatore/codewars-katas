ae65784ae1592a2ac3ce0a6445fd8f0ff5645768
Juan Salvatore <juansalvatore@live.com.ar>
2018-10-19 14:11:14 -0300

    Add functional programming script

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// FUNCTIONAL PROGRAMMING USING JAVASCRIPT
+// Higher order functions:
+// Filter
+// Map
+// Reduce
+// Closures
 const animals = [
   { name: 'Fluffykins', species: 'rabbit' },
   { name: 'Caro', species: 'dog' },
@@ -10,3 +16,52 @@ const animals = [
 let names = animals.map(animal => animal.name)
 
 console.log(names)
+
+
+// Reduce
+const orders = [
+  {amount: 2},
+  {amount: 4},
+  {amount: 4},
+]
+
+const totalAmount = orders.reduce((sum, order) => sum + order.amount, 0)
+console.log(totalAmount)
+
+// Reduce advanced 
+const output = [
+  ['juan', 'perro', '20'],
+  ['juan', 'leon', '20'],
+]
+
+const parsedOutput = output.reduce((owners, line) => {
+  owners[line[0]] = owners[line[0]] || []
+  owners[line[0]].push({animal: line[1], edad: line[2]})
+  return owners
+}, {})
+
+console.log(parsedOutput)
+
+// Closures
+// son funciones que manejan variables independientes
+
+sendRequest = () => {
+  const requestID = '123'
+  $.ajax({
+    url: '/myUrl',
+    success: response => {
+      alert('Request ' + requestID + ' returned')
+    }
+  })
+}
+
+function creaFunc() {
+  var nombre = "Mozilla";
+  function muestraNombre() {
+    return nombre;
+  }
+  return muestraNombre;
+}
+
+var miFunc = creaFunc();
+console.log(miFunc()) 
