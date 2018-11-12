aee9b3f5c3a66253792854460928fa130e6b051e
Juan Salvatore <juansalvatore@live.com.ar>
2018-11-12 15:29:21 -0300

    Add kata

---
+++ 7-kyu/Jaden Casing Strings/index.js
@@ -0,0 +1,6 @@
+String.prototype.toJadenCase = function() {
+  return this.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+}
+
+const str = "How can mirrors be real if our eyes aren't real"
+console.log(str.toJadenCase()) 
